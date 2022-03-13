25b6302cdad4041675a191af1e711da403053f2b
Dan Poulsen <dangjogvara@gmail.com>
2022-03-13 18:41:08 +0000

    implement getAllStudents

--- src/main/java/com/dangjogvara/sms/service/StudentService.java
+++ src/main/java/com/dangjogvara/sms/service/StudentService.java
@@ -1,4 +1,8 @@
 package com.dangjogvara.sms.service;
 
+import com.dangjogvara.sms.entity.Student;
+import java.util.List;
+
 public interface StudentService {
+    List<Student> getAllStudents();
 }
--- src/main/java/com/dangjogvara/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/dangjogvara/sms/service/impl/StudentServiceImpl.java
@@ -1,6 +1,23 @@
 package com.dangjogvara.sms.service.impl;
 
+import com.dangjogvara.sms.entity.Student;
+import com.dangjogvara.sms.repository.StudentRepository;
 import com.dangjogvara.sms.service.StudentService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
