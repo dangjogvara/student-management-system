ffe99f50fabf6c9b426d22845603a7d025c436ad
Dan Poulsen <dangjogvara@gmail.com>
2022-03-13 19:38:31 +0000

    create students for testing

--- src/main/java/com/dangjogvara/sms/StudentManagementSystemApplication.java
+++ src/main/java/com/dangjogvara/sms/StudentManagementSystemApplication.java
@@ -1,13 +1,29 @@
 package com.dangjogvara.sms;
 
+import com.dangjogvara.sms.entity.Student;
+import com.dangjogvara.sms.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
     }
 
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Student student1 = new Student("Dan", "Poulsen", "dangjogvara@gmail.com");
+        studentRepository.save(student1);
+
+        Student student2 = new Student("Durita", "Lamhauge Jóansdóttir", "duritalj@gmail.com");
+        studentRepository.save(student2);
+    }
 }
