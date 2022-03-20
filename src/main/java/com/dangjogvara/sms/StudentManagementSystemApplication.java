f06db734adb407b19fccdf2ac344c9c54d448d6d
Dan Poulsen <dangjogvara@gmail.com>
2022-03-20 19:50:03 +0000

    removed unused code

--- src/main/java/com/dangjogvara/sms/StudentManagementSystemApplication.java
+++ src/main/java/com/dangjogvara/sms/StudentManagementSystemApplication.java
@@ -1,7 +1,5 @@
 package com.dangjogvara.sms;
 
-import com.dangjogvara.sms.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,25 +7,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentManagementSystemApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementSystemApplication.class, args);
+    }
 
-	@Autowired
-	private StudentRepository studentRepository;
+    @Override
+    public void run(String... args) throws Exception {
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		/*
-		Student student1 = new Student("Dan", "Poulsen", "dangjogvara@gmail.com");
-		studentRepository.save(student1);
-
-		Student student2 = new Student("Durita", "Lamhauge Jóansdóttir", "duritalj@gmail.com");
-		studentRepository.save(student2);
-
-		Student student3 = new Student("Irena Louisa", "Duritudóttir", "irenalouisa@gmail.com");
-		studentRepository.save(student3);
-		 */
-	}
+        /*
+         * Student student1 = new Student("Dan", "Poulsen", "dangjogvara@gmail.com");
+         * studentRepository.save(student1);
+         * 
+         * Student student2 = new Student("Durita", "Lamhauge Jóansdóttir",
+         * "duritalj@gmail.com");
+         * studentRepository.save(student2);
+         * 
+         * Student student3 = new Student("Irena Louisa", "Duritudóttir",
+         * "irenalouisa@gmail.com");
+         * studentRepository.save(student3);
+         */
+    }
 }
